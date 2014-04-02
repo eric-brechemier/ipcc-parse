d1a827878b5e2061c68c2dbe6b6c59cb7a78851e
Eric Bréchemier <medea@eric.brechemier.name>
2014-04-02 09:38:07 +0200

    Generate configuration files on the fly
    Each configuration file is now generated before running
    the transformation.
    
    As a result, the relative path to the input and output
    projects shall now be configured in the script parse.sh.

--- parse.sh
+++ parse.sh
@@ -10,8 +10,29 @@
 # change to the script's directory
 cd $(dirname $0)
 
+# create directories for generated configuration files
+mkdir config
+
+createConfig()
+{
+echo "Generate configuration file 'config/$1.properties'"
+cat <<EOF > "config/$1.properties"
+# relative path to input TSV data
+input.file=../ipcc-acquire/output/$1.tsv
+input.file.type=tsv
+
+# relative path for export of the JSONP script
+export.jsonp=../../web/ipcc.projetmedea.fr/data/$1.js
+
+# start and end of wrapper code for JSONP output
+jsonp.start=within("projetmedea.fr",function(publish){publish("$1",
+jsonp.end=);});
+EOF
+}
+
 parse()
 {
+  createConfig "$1"
   ant -f ant/parse.xml -propertyfile "config/$1.properties"
 }
 
